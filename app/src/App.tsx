5c08f1e801942c6ecd480fecec104f87f31630dc
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-08-20 11:28:03 -0700

    First commit

---
+++ app/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Static Web App: React App with Azure Function API
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
