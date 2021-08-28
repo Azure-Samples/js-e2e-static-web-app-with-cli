ecd56e3ca0286526572b7dd5d47ec1600259fc0b
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-08-28 06:16:07 -0700

    api blob get without thumbnails

--- api/upload/index.ts
+++ api/upload/index.ts
@@ -1,17 +1,73 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+// npm dependencies
+import {
+    BlobServiceClient,
+    StorageSharedKeyCredential,
+    newPipeline
+} from '@azure/storage-blob';
+
+// function constants
+const containerName1 = 'thumbnails';
+const containerName2 = 'images';
+const ONE_MEGABYTE = 1024 * 1024;
+const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
+const ONE_MINUTE = 60 * 1000;
+
+// Create storage access
+const sharedKeyCredential = new StorageSharedKeyCredential(
+    process.env.AZURE_STORAGE_ACCOUNT_NAME,
+    process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY);
+const pipeline = newPipeline(sharedKeyCredential);
+const blobServiceClient = new BlobServiceClient(
+    `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+    pipeline
+);
+
+const getBlobName = originalName => {
+    // Use a random number to generate a unique file name, 
+    // removing "0." from the start of the string.
+    const identifier = Math.random().toString().replace(/0\./, '');
+    return `${identifier}-${originalName}`;
+  };
+
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    let bodyResponse;
+    let blobs=[];
+
+    if(req.method.toLowerCase()==="get"){
+        const containerClient = blobServiceClient.getContainerClient(containerName2);
+
+        for await (const blob of containerClient.listBlobsFlat()) {
+            console.log('\t', blob.name);
+            blobs.push(blob.name);
+        }
+
+        bodyResponse = {
+            accountName: process.env.AZURE_STORAGE_ACCOUNT_NAME,
+            containerName: containerName2,
+            thumbnails:null,
+            blobs:blobs
+        }
+
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: bodyResponse
     };
 
 };
 
 export default httpTrigger;
+
+/*
+
+npm install @azure/storage-blob into-stream multer body-parser --save
+
+
+*/
