eb3a5ea7113f0c7be8bcb1c87d34cda978072a8f
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-08-28 17:25:31 -0700

    Upload through function to storage works

--- api/upload/index.ts
+++ api/upload/index.ts
@@ -1,73 +1,43 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import HTTP_CODES from "http-status-enum";
+import * as multipart from "parse-multipart";
 
-// npm dependencies
-import {
-    BlobServiceClient,
-    StorageSharedKeyCredential,
-    newPipeline
-} from '@azure/storage-blob';
-
-// function constants
-const containerName1 = 'thumbnails';
-const containerName2 = 'images';
-const ONE_MEGABYTE = 1024 * 1024;
-const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
-const ONE_MINUTE = 60 * 1000;
-
-// Create storage access
-const sharedKeyCredential = new StorageSharedKeyCredential(
-    process.env.AZURE_STORAGE_ACCOUNT_NAME,
-    process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY);
-const pipeline = newPipeline(sharedKeyCredential);
-const blobServiceClient = new BlobServiceClient(
-    `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
-    pipeline
-);
-
-const getBlobName = originalName => {
-    // Use a random number to generate a unique file name, 
-    // removing "0." from the start of the string.
-    const identifier = Math.random().toString().replace(/0\./, '');
-    return `${identifier}-${originalName}`;
-  };
-
-
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<any> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
 
-    let bodyResponse;
-    let blobs=[];
-
-    if(req.method.toLowerCase()==="get"){
-        const containerClient = blobServiceClient.getContainerClient(containerName2);
-
-        for await (const blob of containerClient.listBlobsFlat()) {
-            console.log('\t', blob.name);
-            blobs.push(blob.name);
-        }
-
-        bodyResponse = {
-            accountName: process.env.AZURE_STORAGE_ACCOUNT_NAME,
-            containerName: containerName2,
-            thumbnails:null,
-            blobs:blobs
-        }
-
+    if (!req.query?.filename) {
+        context.res.body = `Filename is not defined`;
+        context.res.status = HTTP_CODES.BAD_REQUEST
     }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: bodyResponse
-    };
+    if (!req.body || !req.body.length){
+        context.res.body = `Request body is not defined`;
+        context.res.status = HTTP_CODES.BAD_REQUEST
+    }
 
+    try {
+        // Each chunk of the file is delimited by a special string
+        const bodyBuffer = Buffer.from(req.body);
+        const boundary = multipart.getBoundary(req.headers["content-type"]);
+        const parts = multipart.Parse(bodyBuffer, boundary);
+
+        // The file buffer is corrupted or incomplete ?
+        if (!parts?.length){
+            context.res.body = `File buffer is incorrect`;
+            context.res.status = HTTP_CODES.BAD_REQUEST
+        }
+
+        // Actual upload, using an output binding
+        context.bindings.storage = parts[0]?.data;
+    } catch (err) {
+        context.log.error(err.message);
+        {
+            context.res.body = `${err.message}`;
+            context.res.status = HTTP_CODES.INTERNAL_SERVER_ERROR
+        }
+    }
+    return context.res;
 };
 
 export default httpTrigger;
-
-/*
-
-npm install @azure/storage-blob into-stream multer body-parser --save
-
-
-*/
