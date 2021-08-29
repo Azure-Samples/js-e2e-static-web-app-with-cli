5d93980a7b6d27998370377e846d066ee3df414f
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-08-29 09:21:57 -0700

    console.log other details

--- api/upload/index.ts
+++ api/upload/index.ts
@@ -2,6 +2,18 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import HTTP_CODES from "http-status-enum";
 import * as multipart from "parse-multipart";
 
+/*
+Task: accept file, as multipart form request, send file to Azure Storage.
+
+Run the curl command in the same directory as the file:
+
+curl -X POST  -F 'filename=@myfile.jpg' 'http://localhost:7071/api/upload?filename=newfilename.jpg&user=jsmith'
+
+This curl command uses both the querystring and the multi-part form to pass relevant data to and through
+this Azure Function. The querystring property value for `user` becomes the directory name inside the container. 
+The querystring property value for `filename` becomes the file name used in the container. Both these 
+querystring values are used in the `function.json` to construct the container location: `"path": "images/{user}/{filename}",`.
+*/
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<any> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
@@ -27,6 +39,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             context.res.body = `File buffer is incorrect`;
             context.res.status = HTTP_CODES.BAD_REQUEST
         }
+        if(parts[0]?.filename)console.log(`Original filename = ${parts[0]?.filename}`);
+        if(parts[0]?.type)console.log(`Content type = ${parts[0]?.type}`);
+        if(parts[0]?.data?.length)console.log(`Size = ${parts[0]?.data?.length}`);
 
         // Actual upload, using an output binding
         context.bindings.storage = parts[0]?.data;
