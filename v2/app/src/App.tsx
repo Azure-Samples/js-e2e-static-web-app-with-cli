fe46c5560b6685cf5397763428c330357c986e01
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-04-21 10:01:22 +0000

    works

--- v2/app/src/App.tsx
+++ v2/app/src/App.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
+
 import './App.css';
 
 function App() {
 
-  const [users, setUsers] = React.useState([]);
-  const [name, setName] = React.useState('');
-  const [message, setMessage] = React.useState('');
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const mountFlag = useRef(false)
 
-  const getDataFromApi = async(e: any)=>{
-    e.preventDefault();
-    const data = await fetch(`/api/users`);
-    const json = await data.json();
-
-    if (json){
-      setUsers(json);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!mountFlag.current) {
+        mountFlag.current = true;
+        const data = await fetch(`/api/users`);
+        const json = await data.json();
+        setUsers(json);
+      }
     }
-  };
 
-  const sendDataToApi = async(e: any)=>{
+    fetchData();
+  }, []);
+
+  const sendDataToApi = async (e: any) => {
     e.preventDefault();
-    const data = await fetch(`/api/users?user=${name}`);
+    const data = await fetch(`/api/users?user=${name}`, { method: "POST"});
     const returnedName = await data.text();
 
-    if (returnedName){
+    if (returnedName) {
       setMessage(`Hello ${returnedName}`);
     } else {
-      setMessage(`Couldn't send name`); 
+      setMessage(`Couldn't send name`);
+    }
+    const userResponse = await fetch(`/api/users`);
+    const users = await userResponse.json();
+
+    if (users) {
+      setUsers(users);
     }
   };
 
@@ -36,19 +46,20 @@ function App() {
         <p>
           Static Web App: React App with Azure Function API
         </p>
-        <form id="form1" className="App-form" onSubmit={e => getDataFromApi(e)}>
+        <form id="form1" className="App-form" onSubmit={e => sendDataToApi(e)}>
           <div>
-            <input 
-              type="text" 
-              id="name" 
-              className="App-input" 
-              placeholder="Name" 
-              value={name} 
-              onChange={e=>setName(e.target.value)} />
+            <input
+              type="text"
+              id="name"
+              className="App-input"
+              placeholder="Name"
+              value={name}
+              onChange={e => setName(e.target.value)} />
             <button type="submit" className="App-button">Submit</button>
           </div>
         </form>
         <div><h5>Message: {message} </h5></div>
+        <div>{JSON.stringify(users)}</div>
       </header>
     </div>
   );
