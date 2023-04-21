bd0ff5fb0ef4943b5ff9f726859e623e61d1bf90
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2023-04-21 19:10:10 +0000

    clean up

--- v2/app/src/App.tsx
+++ v2/app/src/App.tsx
@@ -1,17 +1,45 @@
 import { useState, useEffect, useRef } from 'react';
+import NavBar from './Components/NavBar';
+import Status from './Components/Status';
 import './App.css';
 
 function App() {
 
+  // auth
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [userName, setUserName] = useState('');
+  const mountFlagAuth = useRef(false)
+
+  // data
   const [users, setUsers] = useState([]);
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
-  const mountFlag = useRef(false)
+  const mountFlagData = useRef(false)
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!mountFlag.current) {
-        mountFlag.current = true;
+      if (!mountFlagAuth.current) {
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+
+        if (clientPrincipal) {
+          setUser(clientPrincipal);
+          userHasAuthenticated(true);
+          setUserName(clientPrincipal?.userDetails.toLowerCase().split(' ').map(x => x[0].toUpperCase() + x.slice(1)).join(' '))
+          console.log(`clientPrincipal = ${JSON.stringify(clientPrincipal)}`);
+        }
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!mountFlagData.current) {
+        mountFlagData.current = true;
         const data = await fetch(`/api/users`);
         const json = await data.json();
         setUsers(json);
@@ -23,11 +51,11 @@ function App() {
 
   const sendDataToApi = async (e: any) => {
     e.preventDefault();
-    const data = await fetch(`/api/users?user=${name}`, { method: "POST"});
+    const data = await fetch(`/api/users?user=${name}`, { method: "POST" });
     const returnedName = await data.text();
 
     if (returnedName) {
-      setMessage(`Hello ${returnedName}`);
+      setMessage(returnedName);
     } else {
       setMessage(`Couldn't send name`);
     }
@@ -41,24 +69,42 @@ function App() {
 
   return (
     <div className="App">
+      <NavBar user={user} />
       <header className="App-header">
-        <p>
-          Static Web App: React App with Azure Function API
-        </p>
         <form id="form1" className="App-form" onSubmit={e => sendDataToApi(e)}>
           <div>
             <input
               type="text"
               id="name"
               className="App-input"
-              placeholder="Name"
+              placeholder="Enter name to add to data list"
               value={name}
               onChange={e => setName(e.target.value)} />
             <button type="submit" className="App-button">Submit</button>
           </div>
         </form>
-        <div><h5>Message: {message} </h5></div>
-        <div>{JSON.stringify(users)}</div>
+        <div><h5>Name added: {message} </h5></div>
+
+        <details>
+          <summary>Public data</summary>
+          <p><h5>Data: {JSON.stringify(users)}</h5></p>
+        </details>
+
+        {isAuthenticated ?
+          <div>
+            <details>
+              <summary>Private data - just for {userName}</summary>
+              <p>
+                <h5>Auth: {isAuthenticated}</h5>
+                <p><Status user={user} /></p>
+              </p>
+            </details>
+            <p>{JSON.stringify(user)}</p>
+          </div>
+          : <div>Sign in for private data access</div>
+        }
+
+
       </header>
     </div>
   );
